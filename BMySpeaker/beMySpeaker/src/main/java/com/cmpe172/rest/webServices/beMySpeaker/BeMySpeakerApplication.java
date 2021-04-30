e123902c72a2171ad852e378b27c2537192a3a02
tienpham0203 <46726728+tienpham0203@users.noreply.github.com>
2021-04-30 11:44:06 -0700

    Add files via upload

---
+++ BMySpeaker/beMySpeaker/src/main/java/com/cmpe172/rest/webServices/beMySpeaker/BeMySpeakerApplication.java
@@ -0,0 +1,13 @@
+package com.cmpe172.rest.webServices.beMySpeaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMySpeakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeMySpeakerApplication.class, args);
+	}
+
+}
