07f66ecfd13bb79ce9f7e20eb3cbf9d648fcd3eb
jing hui <jinghui.ng@sjsu.edu>
2021-05-07 12:35:57 +0800

    Update GridFsDemo.java

--- GridFsDemo.java
+++ GridFsDemo.java
@@ -1,5 +1,6 @@
 package mongoGridFs;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
@@ -7,11 +8,21 @@ import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 import com.mongodb.gridfs.GridFS;
+import com.mongodb.gridfs.GridFSDBFile;
+import com.mongodb.gridfs.GridFSInputFile;
+import com.mongodb.util.JSON;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.bson.Document;
 
 import com.mongodb.MongoClient;
 
@@ -25,20 +36,20 @@ public class GridFSDemo {
         // Create a GridFS collection in the database object  
         GridFS gridFS= new GridFS(db,collectionName);         
 
-        // Create a test file, mongo exists by default  
-        File file=new File("/Users/jinghuing/Downloads/sampleMP3.mp3");  
-        FileInputStream fileInputStream=new FileInputStream(file);  
-        
+//        // Create a test file, mongo exists by default  
+//        File file=new File("/Users/jinghuing/Downloads/newMP3sample.mp3");  
+//        FileInputStream fileInputStream=new FileInputStream(file);  
+//        
 //        // Create a gridFS file data stream  
 //        GridFSInputFile createFile=gridFS.createFile(fileInputStream);  
 //        // Set the file properties  
-//        createFile.put("filename", "sampleMP3");  
-//        createFile.put("text", "UserInputText4");
-//        createFile.put("contentSpeech", "application/mp3"); 
+//        createFile.put("filename", "newMP3sample");  
+//        createFile.put("text", "newText");
+//        createFile.put("contentSpeech", "newMP3sample/mp3"); 
 //        
 //        createFile.save();  
 //        
-//        // according to the id query upload file 
+//        //according to the id query upload file 
 //        GridFSDBFile findOne= gridFS.findOne(new BasicDBObject("_id",createFile.getId()));  
 //        System.out.print(findOne + "\n\n"); 
 //        
@@ -49,8 +60,13 @@ public class GridFSDemo {
             System.out.print(fileList.next());  
         }  
         
+        // This query fetches the files I need
+        BasicDBObject query = new BasicDBObject("text", "UserInputText4");
+        List<GridFSDBFile> files = gridFS.find(query);
+        System.out.println("\n\n" + files);
+       
         //delete files 
-        gridFS.remove(new  BasicDBObject("_id",createFile.getId())); 
+//        gridFS.remove(new  BasicDBObject("_id",createFile.getId())); 
         
         client.close();  
 	   }  
