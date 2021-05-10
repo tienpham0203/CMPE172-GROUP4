6a99d36cf8afb4f983404182d77c2a400f3c50ba
ahabuzaina <33908730+ahabuzaina@users.noreply.github.com>
2021-05-10 03:32:58 +0300

    Add files via upload

--- Polly/src/main/java/com/amazonaws/demos/polly/pollyDemo.java
+++ Polly/src/main/java/com/amazonaws/demos/polly/pollyDemo.java
@@ -1,5 +1,7 @@
 package com.amazonaws.demos.polly;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import com.amazonaws.AmazonServiceException;
@@ -8,15 +10,15 @@ import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.polly.AmazonPollyClient;
+import com.amazonaws.services.polly.AmazonPollyClientBuilder;
 import com.amazonaws.services.polly.model.DescribeVoicesRequest;
 import com.amazonaws.services.polly.model.DescribeVoicesResult;
 import com.amazonaws.services.polly.model.OutputFormat;
 import com.amazonaws.services.polly.model.SynthesizeSpeechRequest;
 import com.amazonaws.services.polly.model.SynthesizeSpeechResult;
 import com.amazonaws.services.polly.model.Voice;
-import javazoom.jl.player.advanced.AdvancedPlayer;
-import javazoom.jl.player.advanced.PlaybackEvent;
-import javazoom.jl.player.advanced.PlaybackListener;
+import com.amazonaws.services.polly.model.VoiceId;
+import com.amazonaws.services.polly.AmazonPolly;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
@@ -24,33 +26,13 @@ import java.util.List;
 
 
 public class pollyDemo {
-	private static String bucket_name = "bucket--s3";
-	private static String file_name = "story1.txt";
-	private final AmazonPollyClient polly;
+	private static String bucket_name = "bucket---s3";
+	private static String file_name = "readme";
+	private static final AmazonPollyClient polly = new AmazonPollyClient(new DefaultAWSCredentialsProviderChain(), 
+			new ClientConfiguration());
 	private final Voice voice;
 	private final static AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_WEST_1).build();
 	private static String Text = null;
-
-	public pollyDemo(Region region) {
-		// create an Amazon Polly client in a specific region
-		polly = new AmazonPollyClient(new DefaultAWSCredentialsProviderChain(), 
-		new ClientConfiguration());
-		// Create describe voices request.
-		DescribeVoicesRequest describeVoicesRequest = new DescribeVoicesRequest();
-
-		// Synchronously ask Amazon Polly to describe available TTS voices.
-		DescribeVoicesResult describeVoicesResult = polly.describeVoices(describeVoicesRequest);
-		voice = describeVoicesResult.getVoices().get(2);
-	}
-
-	public InputStream synthesize(String text, OutputFormat format) throws IOException {
-		SynthesizeSpeechRequest synthReq = 
-		new SynthesizeSpeechRequest().withText(text).withVoiceId(voice.getId())
-				.withOutputFormat(format);
-		SynthesizeSpeechResult synthRes = polly.synthesizeSpeech(synthReq);
-
-		return synthRes.getAudioStream();
-	}
 
 	public static void main(String args[]) throws Exception {
 		//retrieve the String from S3 bucket object
@@ -60,31 +42,9 @@ public class pollyDemo {
             System.err.println(e.getErrorMessage());
             System.exit(1);
         }
-		
-		//create the test class
-		pollyDemo helloWorld = new pollyDemo(Region.getRegion(Regions.US_WEST_1));
-		
-		//get the audio stream
-		InputStream speechStream = helloWorld.synthesize(Text, OutputFormat.Mp3);
-		System.out.println(speechStream);
-		//create an MP3 player
-		AdvancedPlayer player = new AdvancedPlayer(speechStream,
-				javazoom.jl.player.FactoryRegistry.systemRegistry().createAudioDevice());
-
-		player.setPlayBackListener(new PlaybackListener() {
-			@Override
-			public void playbackStarted(PlaybackEvent evt) {
-				System.out.println("Playback started");
-				System.out.println(Text);
-			}
-			
-			@Override
-			public void playbackFinished(PlaybackEvent evt) {
-				System.out.println("Playback finished");
-			}
-		});
-		// play it!
-		player.play();
+		System.out.println(Text);
+		InputStream n = SynthesizeSpeechSample(Text);
+		s3.putObject(bucket_name, "Polly peech", n, null);
 	}
 	
 	public static Bucket getBucket(String bucket_name) {
@@ -98,4 +58,45 @@ public class pollyDemo {
         }
         return named_bucket;
     }
+	
+
+	public static InputStream SynthesizeSpeechSample(String Text){        
+        SynthesizeSpeechRequest synthesizeSpeechRequest = new SynthesizeSpeechRequest()
+                .withOutputFormat(OutputFormat.Mp3)
+                .withVoiceId(VoiceId.Joanna)
+                .withText(Text);
+ 
+            SynthesizeSpeechResult synthesizeSpeechResult = polly.synthesizeSpeech(synthesizeSpeechRequest);
+            InputStream in = synthesizeSpeechResult.getAudioStream();
+//            try (InputStream in = synthesizeSpeechResult.getAudioStream()){
+//                while ((readBytes = in.read(buffer)) > 0) {
+//                    outputStream.write(buffer, 0, readBytes);
+//                }
+//                return in;
+//            }
+//        } catch (Exception e) {
+//            System.err.println("Exception caught: " + e);
+        return in;     
+}
+	
+	public pollyDemo(Region region) {
+
+		DescribeVoicesRequest describeVoicesRequest = new DescribeVoicesRequest();
+
+		// Synchronously ask Amazon Polly to describe available TTS voices.
+		DescribeVoicesResult describeVoicesResult = polly.describeVoices(describeVoicesRequest);
+		voice = describeVoicesResult.getVoices().get(2);
+	}
+	
+	
+	
+
+	public InputStream synthesize(String text, OutputFormat format) throws IOException {
+		SynthesizeSpeechRequest synthReq = 
+		new SynthesizeSpeechRequest().withText(text).withVoiceId(voice.getId())
+				.withOutputFormat(format);
+		SynthesizeSpeechResult synthRes = polly.synthesizeSpeech(synthReq);
+
+		return synthRes.getAudioStream();
+	}
 }
